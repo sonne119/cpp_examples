895a63ac4c73a7ca8f0f6076964666a31db6bbaa
sonne119 <immerguten@ukr.net>
2025-09-11 13:40:12 +0300

    was added entry point

--- examples/custom_unique_ptr/custom_unique_ptr.cpp
+++ examples/custom_unique_ptr/custom_unique_ptr.cpp
@@ -61,3 +61,23 @@ struct Temp {
     void Hello() { std::cout << "Hello temp!\n"; }
 };
 
+int main() {
+	MyUniquePtr<Temp> ptr1(new Temp());
+	ptr1->Hello();
+
+	MyUniquePtr<Temp> ptr2 = std::move(ptr1);
+	
+    if (!ptr1.Get()) {
+		std::cout << "ptr1 was moved to ptr2 and now is null \n";
+	}
+
+	Temp* raw_ptr = ptr2.Release();
+	
+    if (!ptr2.Get()) {
+		std::cout << "ptr2 was released and pass owner to raw_ptr  and now is null \n";
+	}
+	raw_ptr->Hello(); // call method from raw pointer
+
+	return 0;
+}
+
