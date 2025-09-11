86f4a1ac01973b4e7c72437de8685e572752b426
sonne118 <_sonne_@ukr.net>
2025-09-11 12:00:37 +0300

    unique_ptr

---
+++ examples/custom_unique_ptr/custom_unique_ptr.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+template <typename T>
+class MyUniquePtr {
+private:
+    T* ptr; 
+
+public:
+   
+    explicit MyUniquePtr(T* p = nullptr) : ptr(p) {}
+
+   
+    MyUniquePtr(const MyUniquePtr&) = delete;
+    MyUniquePtr& operator=(const MyUniquePtr&) = delete;
+
+    
+    MyUniquePtr(MyUniquePtr&& other) noexcept : ptr(other.ptr) {
+        other.ptr = nullptr;
+    }
+
+    MyUniquePtr& operator=(MyUniquePtr&& other) noexcept {
+        if (this != &other) {
+            delete ptr;        
+            ptr = other.ptr;   
+            other.ptr = nullptr;
+        }
+        return *this;
+    }
+
+  
+    ~MyUniquePtr() {
+        delete ptr;
+    }
+
+
+    T& operator*() const {
+        return *ptr;
+    }
+
+
+    T* operator->() const {
+        return ptr;
+    }
+
+    T* Get() const {
+        return ptr;
+    }
+
+    T* Release() {
+        T* temp = ptr;
+        ptr = nullptr;
+        return temp;
+    }
+};
+
+
+struct Test {
+    void Hello() { std::cout << "Hello from Test!\n"; }
+};
+
